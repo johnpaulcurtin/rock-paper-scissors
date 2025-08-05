f3bdb22c627a4227543817bbf6e87417d14de3c6
johnpaulcurtin <juanlove@gmail.com>
2025-08-04 17:07:46 -0700

    create getComputerChoice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,21 @@
 console.log ("Hello,World!")
+
+
+function getComputerChoice(){
+    const num = Math.random()
+
+    if (num<0.33) {
+        return "rock";
+    }
+    else if(num>0.66) {
+        return "paper";
+    }
+    else
+    {return "scissors"}
+    
+}
+
+console.log(getComputerChoice());
+
+
+
