b65ecd885bf64ac7d7fa555d9f1b6da7830821f7
johnpaulcurtin <juanlove@gmail.com>
2025-08-07 16:26:52 -0700

    fix function playRound checkWinner

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,20 +1,58 @@
 console.log ("Hello,World!")
 
+const weapon = ['rock', 'paper', 'scissors'];
+const playerSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(){
+    const winner = checkWinner(playerSelection, computerSelection);
+    if(winner == 'Tie') {
+        return 'It\'s a tie.';
+    }
+    else if(winner == 'Player'){
+        return `You win. ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You lose. ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
+console.log(playRound())
+
+
+
 
 function getComputerChoice(){
-    const num = Math.random()
-
-    if (num<0.33) {
-        return "rock";
-    }
-    else if(num>0.66) {
-        return "paper";
-    }
-    else
-    {return "scissors"}
-    
+    return weapon[Math.floor(Math.random()*weapon.length)];    
 }
 
 
+function getHumanChoice(){
+    let input = prompt("Choose your weapon: rock, paper, or scissors");
+        input = input.toLowerCase();  
+        return input;
 }
 
+
+function checkWinner() {
+    if (playerSelection == computerSelection) {
+        return 'Tie';
+    } else if (
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+    ) {
+        return 'Player';
+    } else{
+        return 'Computer';
+    }
+}
+console.log (checkWinner())
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+console.log (computerSelection)
+console.log (playerSelection)
